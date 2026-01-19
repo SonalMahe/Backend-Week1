772bdc419535b49f4b38d2d283972ff5b2c769e5
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-01-19 13:19:27 +0100

    added extra variable for practice
    -added age variable

--- homework.js
+++ homework.js
@@ -1,4 +1,14 @@
-const firstName ="Sonal";
+const firstName = "Sonal";
 const favNumber = 9;
 const islikesCoding = true;
-console.log(`Hi my name is ${firstName}, my favourite number is ${favNumber} and it is ${islikesCoding} ,that i like coding.`);
+// console.log(`Hi my name is ${firstName}, my favourite number is ${favNumber} and it is ${islikesCoding} ,that i like coding.`);
+
+//extra practice-
+const birthYear = 1987;
+const currentYear = 2026;
+const age = currentYear - birthYear;
+console.log
+    (`Hi my name is ${firstName}, 
+    my favourite number is ${favNumber} 
+    and it is ${islikesCoding}, that i like coding.
+   and my age is ${age}.`);

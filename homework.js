35d94336624f20dc2e55e3287ed487b562d89006
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-01-19 13:11:29 +0100

    added comma

--- homework.js
+++ homework.js
@@ -1,4 +1,4 @@
 const firstName ="Sonal";
 const favNumber = 9;
 const islikesCoding = true;
-console.log(`Hi my name is ${firstName}, my favourite number is ${favNumber} and it is ${islikesCoding} that i like coding`);
+console.log(`Hi my name is ${firstName}, my favourite number is ${favNumber} and it is ${islikesCoding} ,that i like coding.`);

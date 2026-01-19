f8ea2c351af08552ceaa637c49a9fad50fe2e9e7
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-01-19 13:31:47 +0100

    extra exercise
    - made arrays and object
    -create student object

--- homework.js
+++ homework.js
@@ -11,4 +11,16 @@ console.log
     (`Hi my name is ${firstName}, 
     my favourite number is ${favNumber} 
     and it is ${islikesCoding}, that i like coding.
-   and my age is ${age}.`);
+   and I am ${age} years old.`);
+
+//Advanced or more exercise-
+let foods = ["Pasta", "ThaiCurry", "Pizza"];
+let student = {
+    lastName: "Maheshwari",
+    favouriteNumber: 7,
+    islikesCoding: false,
+    foods: foods
+};
+
+console.log(student);            //whole object
+console.log(student.lastName);  //one property
